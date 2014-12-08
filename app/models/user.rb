c23031aeab82df54df357ecf635e459d4ab4dea5
jKodrum <jkodrum@gmail.com>
2014-12-09 00:32:36 +0800

    create Friendships relation
    rails g model friendship
    there is a friendship between a user and another user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, presence: true
+
+  has_many :friendships
+  has_many :relations, through: :friendships, source: :friend
+  has_many :friends, -> { where(friendships: { status: 'accepted' }) }, through: :friendships, source: :friend
+  has_many :pending_friends, -> { where(friendships: { status: 'pending' }) }, through: :friendships, source: :friend
+  has_many :requested_friends, -> { where(friendships: { status: 'requested' }) }, through: :friendships, source: :friend
+
+  def has_relation_of?(@other)
+    self.relations.include?(@other)
+  end
 end
