bf064699b5cc6e3859765d8d64ef5eace17f19d1
jKodrum <jkodrum@gmail.com>
2014-12-18 03:32:33 +0800

    add post and user dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
            -> { where(friendships: { status: 'requested' }) },
            through: :friendships,
            source: :friend
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true
 
